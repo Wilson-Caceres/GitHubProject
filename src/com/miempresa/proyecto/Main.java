6207ce48e037a507fce1179c25e0a070fa1750a5
Wilson-Caceres <wcaceres@insgabrielamistral.cat>
2025-02-20 13:03:16 +0100

    2a modificación

--- src/com/miempresa/proyecto/Main.java
+++ src/com/miempresa/proyecto/Main.java
@@ -3,5 +3,8 @@ package com.miempresa.proyecto;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola, Git!");
+
+        System.out.println("2a modificación GitHub");
+
     }
 }
